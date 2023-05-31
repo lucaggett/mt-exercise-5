50e747eaa6590dae671efe7acc78b6f4aa40a319
Luc <luc@aggett.com>
2023-06-01 00:48:03 +0200

    Add command-line arguments

--- scripts/create_bpe.sh
+++ scripts/create_bpe.sh
@@ -1,9 +1,26 @@
-# subword-nmt learn-joint-bpe-and-vocab --input {train_file}.de {train_file}.it -s {num_operations} -o {codes_file} --write-vocabulary {vocab_file}.de {vocab_file}.it
-# apply to train, dev, test
-
 # Path: scripts/apply_bpe.sh
 # Data is found in data/{train,dev,test}.de-it.{de,it}
-# Codes and vocab are found in data/bpe/codes.{de,it} and data/bpe/vocab.{de,it}
+# Codes and vocab are found in data/$directory/codes.{de,it} and data/$directory/vocab.{de,it}
+
+# Get arguments (directory and number of codes)
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    echo "Usage: ./create_bpe.sh <directory> <num_codes>"
+    exit 1
+fi
+if [ $# -eq 1 ]; then
+    num_codes=2000
+    echo "Using default number of codes: 2000"
+else
+    num_codes=$2
+    echo "Using number of codes: $num_codes"
+fi
+if [ $# -eq 2 ]; then
+    directory=$1
+    echo "Using directory: $directory"
+else
+    directory="bpe_$num_codes"
+    echo "Using default directory: $directory"
+fi
 
 # check if bpe directory exists
 if [ ! -d "data/bpe" ]; then
@@ -12,7 +29,32 @@ fi
 
 # learn BPE from concatenated training data
 # create concatenated training data
-cat data/processed/train.de-it.de.* data/processed/train.de-it.it.* > data/bpe/train.de-it.de-it
-cat data/processed/train.de-it.it.* data/processed/train.de-it.de.* > data/bpe/train.de-it.it-de
-# learn BPE
-subword-nmt learn-joint-bpe-and-vocab --input data/bpe/train.de-it.de-it data/bpe/train.de-it.it-de -s 32000 -o data/bpe/codes.de-it --write-vocabulary data/bpe/vocab.de-it.de data/bpe/vocab.de-it.it
+cat data/processed/train.de-it.de.* data/processed/train.de-it.it.* > data/$directory/train.de-it.de-it
+cat data/processed/train.de-it.it.* data/processed/train.de-it.de.* > data/$directory/train.de-it.it-de
+# learn BPE only if codes file does not exist
+if [ ! -f "data/$directory/codes.de-it" ]; then
+    subword-nmt learn-joint-bpe-and-vocab --input data/$directory/train.de-it.de-it data/$directory/train.de-it.it-de -s $num_codes -o data/$directory/codes.de-it --write-vocabulary data/$directory/vocab.de-it.de data/$directory/vocab.de-it.it
+fi
+echo "BPE learned"
+# apply BPE to train, dev and test
+for lang in de it; do
+    for split in train dev test; do
+        subword-nmt apply-bpe -c data/$directory/codes.de-it --vocabulary data/$directory/vocab.de-it.$lang --vocabulary-threshold 50 < data/processed/$split.de-it.$lang.tokenized > data/$directory/$split.de-it.$lang.bpe
+        echo "BPE applied to $split.$lang"
+        echo "saved in data/$directory/$split.de-it.$lang.bpe"
+    done
+done
+
+# create combined vocab if it does not exist
+if [ ! -f "data/$directory/combined_vocab.txt" ]; then
+    cat data/$directory/vocab.de-it.de data/$directory/vocab.de-it.it > data/$directory/combined_vocab.txt
+    # remove counts
+    awk '{print $1}' data/$directory/combined_vocab.txt > data/$directory/combined_vocab.txt.tmp && mv data/$directory/combined_vocab.txt.tmp data/$directory/combined_vocab.txt
+fi
+
+# rename files
+for lang in de it; do
+    for split in train dev test; do
+        mv data/$directory/$split.de-it.$lang.bpe data/$directory/$split.$lang
+    done
+done
